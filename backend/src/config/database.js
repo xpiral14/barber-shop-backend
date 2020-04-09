f4cbe1dcb664dcbbc54a4d94ab469193fa2bc706
xpiral14 <sa.reis1999@gmail.com>
2020-04-08 21:08:55 -0300

    changed the db timezone

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 module.exports = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  logging: process.env.NODE_ENV === "development" ? console.log : false,
-  timezone: "Etc/GMT-3",
-  dialect: "mariadb",
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialect: 'mariadb',
   dialectOptions: {
-    timezone: "Etc/GMT-3"
+    timezone: 'Etc/GMT-3',
   },
   define: {
     timestamps: true,
     underscored: false,
-    underscoredAll: false
-  }
+    underscoredAll: false,
+  },
+  timezone: '+6:00',
 };
