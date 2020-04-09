6f86ccd1ce4ff0d19e1849fbf820e9afcb10c93c
xpiral14 <sa.reis1999@gmail.com>
2020-04-08 21:24:03 -0300

    feat: added some others validations

--- backend/src/middlewares/validate.js
+++ backend/src/middlewares/validate.js
@@ -1,75 +1,75 @@
-import { string, object, number } from "yup";
-import {
-  requiredField,
-  invalidField,
-  minFieldSize,
-  maxFieldSize,
-} from "../constants/messages";
-import validateCNPJ from "../utils/validateCNPJ";
-
+import { string, object, number, date } from 'yup';
+import { requiredField, invalidField, minFieldSize, maxFieldSize } from '../constants/messages';
+import validateCNPJ from '../utils/validateCNPJ';
+import { startOfHour, parseISO, isAfter } from 'date-fns';
 export default function validate(fields = []) {
   const allFields = {
-    genderId: number().required(requiredField("código do gênero")).default(1),
-    name: string().required(requiredField("Nome")),
-    fantasyName: string().required(requiredField("nome fantasia")),
-    userTypeId: number().default(1).required(requiredField("tipo de usuário")),
+    appointment: string()
+      .required(requiredField('agendamento'))
+      .test('ValidAppointment', invalidField('agendamento'), function (date) {
+        let hourStart = startOfHour(parseISO(date));
+        return isAfter(hourStart, new Date());
+      })
+      .transform(function (value) {
+        return startOfHour(parseISO(value)).toISOString().replace('T', ' ');
+      }),
+    genderId: number().required(requiredField('código do gênero')).default(1),
+
+    clientId: number().required(requiredField('código do cliente')),
+
+    employeeId: number().required(requiredField('código do barbeiro')),
+    name: string().required(requiredField('Nome')),
+
+    fantasyName: string().required(requiredField('nome fantasia')),
+
+    userTypeId: number().default(1).required(requiredField('tipo de usuário')),
     logo: string(),
 
     cnpj: string()
-      .test(
-        "cnpjValidation",
-        invalidField("CNPJ"),
-        (value) => value && validateCNPJ(value)
-      )
+      .test('cnpjValidation', invalidField('CNPJ'), (value) => value && validateCNPJ(value))
       .transform(function (value, originalValue) {
-        return (value = value?.replace(/\D/g, ""));
+        return (value = value?.replace(/\D/g, ''));
       }),
 
     logo: string(),
 
-    email: string()
-      .email(invalidField("Email"))
-      .required(requiredField("Email")),
+    email: string().email(invalidField('Email')).required(requiredField('Email')),
 
-    password: string()
-      .min(3, minFieldSize("senha", 3))
-      .required(requiredField("senha")),
+    password: string().min(3, minFieldSize('senha', 3)).required(requiredField('senha')),
 
-    companyId: number().required(requiredField("codigo da empresa")),
+    companyId: number().required(requiredField('codigo da empresa')),
 
     cep: string()
-      .required(requiredField("CEP"))
-      .min(8, invalidField("CEP"))
+      .required(requiredField('CEP'))
+      .min(8, invalidField('CEP'))
       .transform(function (value, originalValue) {
-        return (value = value.replace(/\D/g, ""));
+        return (value = value.replace(/\D/g, ''));
       }),
 
-    state: string().max(30, maxFieldSize("estado", 20)),
+    state: string().max(30, maxFieldSize('estado', 20)),
 
-    city: string().max(30, maxFieldSize("cidade", 20)),
+    city: string().max(30, maxFieldSize('cidade', 20)),
 
-    neighborhood: string().max(30, maxFieldSize("bairro", 20)),
+    neighborhood: string().max(30, maxFieldSize('bairro', 20)),
 
-    street: string().max(30, maxFieldSize("rua", 20)),
+    street: string().max(30, maxFieldSize('rua', 20)),
 
-    number: string().max(30, maxFieldSize("número", 20)),
+    number: string().max(30, maxFieldSize('número', 20)),
 
     phone: string()
       .matches(
         /^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?(9|9[ .-])?[2-9]\d{3}[ .-]?\d{4}$/gm,
-        invalidField("telefone")
+        invalidField('telefone')
       )
-      .required(requiredField("Telefone"))
+      .required(requiredField('Telefone'))
       .transform(function (value, originalValue) {
         // if(this.isType(value)) return value
-        return (value = value.replace(/\D/g, ""));
+        return (value = value.replace(/\D/g, ''));
       }),
-    userId: number().required(requiredField("código do usuário")),
-    serviceId: number().required(requiredField("código do serviço")),
+    userId: number().required(requiredField('código do usuário')),
+    serviceId: number().required(requiredField('código do serviço')),
 
-    price: number()
-      .max(10000, maxFieldSize("preço", 10000))
-      .required(requiredField("preço")),
+    price: number().max(10000, maxFieldSize('preço', 10000)).required(requiredField('preço')),
   };
 
   let objectSchema = {};
