cfc539a1f1a39de2837d5dce2ea4d416654ff86a
Samuel Reis <samuel.reis@gmail.com>
2020-12-31 14:31:21 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+
+function App() {
+	return (
+		<Button variant="contained" color="secondary">
+      Hello World
+		</Button>
+	)
+}
+
+export default App
