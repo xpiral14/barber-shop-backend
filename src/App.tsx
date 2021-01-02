b4c470b94a0a8e855052cace8b786545a91d36af
Erick Cesar <e.cesar88@gmail.com>
2021-01-02 12:02:57 -0500

    Render navigation and global bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
-import Login from './Pages/Login'
-function App() {
-	return <Login />
+//import Login from './Pages/Login'
+//import Dashboard from './Pages/Dashboard'
+import NavigationBar from './Components/NavigationBar'
+import GlobalBar from './Components/GlobalBar'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+const GlobalAppTheme = {
+  backgroundColor: '#ff0000',
+  fontFamily: 'Roboto',
+  borderRadius: '5px',
+  spacing: '5px',
+}
+
+const App: React.FC = () => {
+  return (
+    <>
+      <ThemeProvider theme={GlobalAppTheme}>
+        <GlobalBar />
+        <NavigationBar />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App
