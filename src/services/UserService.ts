ce906f0eb841768888ab69bd3a854bb8aba87282
Samuel Reis <sa.reis1999@gmail.com>
2021-01-03 12:13:49 -0300

    Created UserService

---
+++ src/services/UserService.ts
@@ -0,0 +1,16 @@
+import api from '../Config/api'
+import User from '../Models/User'
+
+export default class CompanyService {
+  static defaultPath = '/user'
+  static async getAll() {
+    const response = api.get<User[]>(this.defaultPath)
+
+    return (await response).data
+  }
+
+  static async getOne(userId: number) {
+    const response = api.get<User>(`${this.defaultPath}/${userId}`)
+    return (await response).data
+  }
+}
