1d52e19d70d526075edf62ccadcccad3c293823a
Erick Cesar <e.cesar88@gmail.com>
2021-01-04 07:40:33 -0500

    Added vehicle type

---
+++ src/Models/Vehicle.ts
@@ -0,0 +1,12 @@
+type Vehicle = {
+  id: number,
+  make: string
+  model: string
+  color: string
+  year: number
+  kmDriven: number
+  licensePlate: string
+  fuelType: string
+}
+
+export default Vehicle
