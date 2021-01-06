b3950202c43d3a3fa91b1c1f070e32c8fe1a683b
Erick Cesar <e.cesar88@gmail.com>
2021-01-05 20:55:09 -0500

    Added VehicleService

--- src/services/VehicleService.ts
+++ src/services/VehicleService.ts
@@ -3,7 +3,7 @@ import Metadata from '../Models/Metadata'
 import Vehicle from '../Models/Vehicle'
 
 export default class VehicleService {
-  static defaultPath = '/company/vehicle/'
+  static defaultPath = '/company/vehicle'
 
   static async create() {
     const response = api.post<Vehicle[]>(`${this.defaultPath}`)
@@ -25,13 +25,13 @@ export default class VehicleService {
     return (await response).data
   }
 
-  static async getOne(companyId: number) {
-    const response = api.get<Vehicle>(`${this.defaultPath}/${companyId}`)
+  static async getOne(vehicleId: number) {
+    const response = api.get<Vehicle>(`${this.defaultPath}/${vehicleId}`)
     return (await response).data
   }
 
-  static async update(companyId?: number, vehicleData?: Vehicle) {
-    await api.put(`${this.defaultPath}/${companyId}`, vehicleData)
+  static async update(vehicleId?: number, vehicleData?: Vehicle) {
+    await api.put(`${this.defaultPath}/${vehicleId}`, vehicleData)
     return true
   }
 }
