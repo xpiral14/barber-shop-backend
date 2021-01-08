fb4d69d11853987c8f6f9d5cedb7a049bda15972
Erick Cesar <e.cesar88@gmail.com>
2021-01-07 22:05:59 -0500

    Added breadcrumbs

--- src/Pages/Vehicles/VehicleDetails.tsx
+++ src/Pages/Vehicles/VehicleDetails.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Vehicle from '../../Models/Vehicle'
-import VehicleService from '../../services/VehicleService'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import { useHistory, useRouteMatch } from 'react-router-dom'
-import Typography from '@material-ui/core/Typography'
-import { default as NumberFormat } from 'react-number-format'
-import formatLicensePlate from '../../Util/formatLicensePlate'
+import { Button, Card, Grid, IconButton, Typography } from '@material-ui/core/'
+import Breadcrumbs from '@material-ui/core/Breadcrumbs'
 import Divider from '@material-ui/core/Divider'
+import Link, { LinkProps } from '@material-ui/core/Link'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  ArrowBack as ArrowBackIcon,
+  Edit as EditIcon,
+  Person as PersonIcon,
+} from '@material-ui/icons/'
+import React, { useEffect, useState } from 'react'
+import { default as NumberFormat } from 'react-number-format'
+import { Link as RouterLink, useHistory, useRouteMatch } from 'react-router-dom'
+import { fuelType } from '../../Contracts/Enums'
+import Vehicle from '../../Contracts/Models/Vehicle'
+import VehicleService from '../../services/VehicleService'
+import formatLicensePlate from '../../Util/formatLicensePlate'
 
 const useStyles = makeStyles({
   root: {
@@ -17,14 +23,13 @@ const useStyles = makeStyles({
     maxWidth: 345,
   },
   grid: {
-    // height: document.documentElement.clientWidth
     height: '100%',
     width: '100%',
   },
   card: {
     width: '100%',
-    height: '300px',
-    padding: '15px',
+    height: '100%',
+    padding: '2rem',
   },
   paper: {
     height: 140,
@@ -47,10 +52,22 @@ const useStyles = makeStyles({
   media: {
     width: '100%',
   },
-  span: {
+  description: {
     marginTop: '0.5rem',
   },
+  breadcrumbCurrent: {
+    color: '#000000',
+  },
 })
+
+interface LinkRouterProps extends LinkProps {
+  to: string
+  replace?: boolean
+}
+
+const LinkRouter = (props: LinkRouterProps) => (
+  <Link {...props} component={RouterLink as any} />
+)
 
 const VehicleDetails: React.FC = () => {
   //eslint-disable-next-line
@@ -70,6 +87,28 @@ const VehicleDetails: React.FC = () => {
     <Grid container spacing={3} className={classes.grid}>
       {vehicle && (
         <>
+          <Grid container xs={12} alignItems='center'>
+            <div>
+              <IconButton aria-label='backOnePage'>
+                <ArrowBackIcon />
+              </IconButton>
+            </div>
+
+            <div>
+              <Breadcrumbs aria-label='breadcrumb'>
+                <LinkRouter color='inherit' to='/empresa'>
+                  Minha Empresa
+                </LinkRouter>
+                <LinkRouter color='inherit' to='/veiculos'>
+                  Veículos
+                </LinkRouter>
+                <span
+                  className={classes.breadcrumbCurrent}
+                >{`${vehicle?.make} ${vehicle?.model}`}</span>
+              </Breadcrumbs>
+            </div>
+          </Grid>
+
           <Grid item xs={12} sm={6}>
             <Card
               className={classes.card}
@@ -83,39 +122,89 @@ const VehicleDetails: React.FC = () => {
             ></Card>
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} direction='column'>
             <Card className={classes.card}>
-              <Typography variant='h4' component='h4'>
-                <b>{`${vehicle?.make} ${vehicle?.model}`}</b>
-              </Typography>
-              <Divider />
-              <div className={classes.span}>
-                <span className={classes.span}>
-                  Ano: <b>{vehicle?.year} </b>/ Modelo: <b>{vehicle?.year}</b>
-                </span>
-                <br />
-                <span className={classes.span}>
-                  Placa: <b>{formatLicensePlate(vehicle?.licensePlate)}</b>
-                </span>
-                <br />
-                <span className={classes.span}>
-                  <b>
-                    <NumberFormat
-                      value={vehicle?.kmDriven}
-                      displayType={'text'}
-                      format='## ### ### ###'
-                      thousandSeparator={true}
-                      decimalSeparator={'.'}
-                      decimalScale={10}
-                      allowEmptyFormatting={true}
-                    />
-                  </b>
-                  km
-                </span>
-                <br />
-                <span className={classes.span}>
-                  Combustível: <b>{vehicle?.fuelType} </b>
-                </span>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Grid item xs={12}>
+                  <Typography variant='h4' component='h4'>
+                    <b>{`${vehicle?.make} ${vehicle?.model}`}</b>
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant='body2'
+                    color='textSecondary'
+                  >
+                    ID: {`${vehicle?.id}`}
+                  </Typography>
+
+                  <Divider />
+                  <div className={classes.description}>
+                    <div className={classes.description}>
+                      Ano: <b>{vehicle?.year} </b>/ Modelo:{' '}
+                      <b>{vehicle?.yearModel}</b>
+                    </div>
+
+                    <div className={classes.description}>
+                      Cor: <b>{vehicle?.color.toUpperCase()} </b>
+                    </div>
+
+                    <div className={classes.description}>
+                      Placa: <b>{formatLicensePlate(vehicle?.licensePlate)}</b>
+                    </div>
+
+                    <div className={classes.description}>
+                      Quilometragem:
+                      <b>
+                        <NumberFormat
+                          value={vehicle?.kmDriven}
+                          displayType={'text'}
+                          thousandSeparator={' '}
+                        />
+                      </b>
+                      km
+                    </div>
+                    <div className={classes.description}>
+                      Combustível:{' '}
+                      <b>
+                        {fuelType[vehicle?.fuelType as keyof typeof fuelType]}{' '}
+                      </b>
+                    </div>
+                  </div>
+                </Grid>
+
+                <div
+                  style={{
+                    marginTop: '15px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <div>
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      startIcon={<PersonIcon />}
+                    >
+                      Proprietário
+                    </Button>
+                  </div>
+
+                  <div>
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      startIcon={<EditIcon />}
+                    >
+                      Editar
+                    </Button>
+                  </div>
+                </div>
               </div>
             </Card>
           </Grid>
