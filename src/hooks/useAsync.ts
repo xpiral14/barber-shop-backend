3cafc5f1cf6088eee5f5416919ea3c2ffb1ab533
xpiral14 <samuel.reis@gmail.com>
2021-03-18 23:38:33 -0300

    Added use  Async hook

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -3,20 +3,21 @@ import { useEffect, useState } from 'react'
 export default function useAsync(asyncFunction: any, deps: any[]) {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-  useEffect(() => {
-    (async () => {
-      setLoading(true)
-      setTimeout(async () => {
-        try {
-          await asyncFunction()
-        } catch (err) {
-          setError(err)
-        } finally {
-          setLoading(false)
-        }
-      }, 500)
-    })()
+  const asyncFunctionWithLoad = async () => {
+    setLoading(true)
+    setTimeout(async () => {
+      try {
+        await asyncFunction()
+      } catch (err) {
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }, 500)
+  }
+  useEffect(() => { 
+    asyncFunctionWithLoad()
   }, deps)
-
-  return [loading, error]
+  const returnValue : [boolean, () => Promise<void>, any] = [loading, asyncFunctionWithLoad, error]
+  return returnValue
 }
