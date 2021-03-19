b0a141c8c61d59ae60e534171c9adc6a2b84539a
xpiral14 <samuel.reis@gmail.com>
2021-03-18 23:48:54 -0300

    Added last line

--- src/Contracts/Models/OrderPiece.ts
+++ src/Contracts/Models/OrderPiece.ts
@@ -1,11 +1,11 @@
 import Piece from './Piece'
 
 export default interface OrderPiece {
-    id: number;
-    orderId: number;
-    pieceId: number;
-    quantity: number;
-    created_at: string;
-    updated_at: string;
-    piece: Piece;
+  id: number
+  orderId: number
+  pieceId: number
+  quantity: number
+  created_at: string
+  updated_at: string
+  piece: Piece
 }
